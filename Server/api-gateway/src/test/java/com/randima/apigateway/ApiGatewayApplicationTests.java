613fac9d093f7dd29acac7c447a7924968e17eff
randima <randimadissanayake.rkd@gmail.com>
2020-03-11 22:19:26 +0530

    Updated

---
+++ Server/api-gateway/src/test/java/com/randima/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randima.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
